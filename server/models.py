8c43be525811868f3ffff6a98fce7039aeaca9a0
caroline opiyo <opiyocaroline20@gmail.com>
2024-01-14 02:11:06 +0300

    python-p4-adding-react-to-flask

--- server/models.py
+++ server/models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
-
+#class
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
